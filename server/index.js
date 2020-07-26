6bcb0957e16943aba1ae9fdbebdbb730c0423ddd
Cristian Orozco <cristianorozco1984@gmail.com>
2020-07-25 23:25:42 -0500

    server connected

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const { addUser, removeUser, getuser, getUserInRoom } = require('./users')
+
+const PORT = process.env.PORT || 5000
+const router = require('./router')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room })
+
+		if (error) return callback(error)
+
+		socket.join(user.room)
+	})
+
+	socket.on('disconnect', () => {
+		console.log('User had left!!!')
+	})
+})
+
+app.use(router)
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT} `))
